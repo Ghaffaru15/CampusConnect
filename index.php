83eb69eba63c47167028bc9fff24d31a9dd7cc69
Ghaffaru <mudashiruagm@gmail.com>
2018-06-29 02:16:46 +0300

    Home Page

---
+++ index.php
@@ -0,0 +1,81 @@
+<?php
+	$page_title = 'Campus Connect';
+	include("Includes/header.html");
+?>
+
+    <div id="slider">
+      <figure>
+        <a href=""><img src="Includes/ccslideimg1.jpg"></a>
+        <a href=""><img src="Includes/ccslideimg2.jpg"></a>
+        <a href=""><img src="Includes/ccslideimg3.jpg"></a>
+        <a href=""><img src="Includes/ccslideimg4.jpg"></a>
+        <a href=""><img src="Includes/ccslideimg5.jpg"></a>
+      </figure>
+    </div>
+
+    <div class="container">
+      <div class="hmeintro">
+        <h1>Are you a student? Find everything you need here!</h1>
+        <ul>
+          <li>it's Safe</li>
+          <li>it's Quality</li>
+          <li>it's Affordable</li>
+          <li>it's Convenient</li>
+          <li id="itsforyou">it's for YOU!</li>
+        </ul>
+      </div>
+
+
+      <div class="hmecat">
+        <div class="product hmecontent">
+          <p>Enter here to find all kinds of products
+          and services available on your campus</p>
+        </div>
+        <a href=""><div class="prodtxt  prodimg product hmecontent  text" >
+           PRODUCTS
+        </div></a>
+        <div class="quiz hmecontent">
+          <p>Enter here to find well compiled practice
+             questions pack on various topics and courses
+             from qualified personnel across campuses</p>
+        </div>
+        <a href=""><div class="quiztxt quizimg quiz hmecontent text">
+          QUIZ PACKS
+        </div></a>
+      </div>
+
+
+      <div class="messages">
+        <div class="hmemsgs qltyassurance">
+          <h1>Quality Assurance</h1>
+          <p>Our Quality Management Team works efficiently
+          to ensure that we bring you only the best quality
+          materials. Our quiz packs are prepared by qualified
+          personnel who are at the time of submission in the
+          teaching bussiness. </p>
+        </div>
+
+        <div class="hmemsgs expertswords">
+          <h1>Take it from the experts</h1>
+          <p>Hello there!</p>
+          <p>I highly recommend Campus Connect's materials
+           to any student who is preparing for exams. I have
+           had the opportunity to examine  the contents of
+           some of the materials, it was all excellently made.</p>
+           <p>............Dr. Someone Some, Economics Lecturer, UG</p>
+        </div>
+
+        <div class="hmemsgs convenient">
+          <h1>Conveniently Made</h1>
+          <p>This platform has been created in such a way to
+          make it's use highly convenient. Products are well
+          categorized and Quiz packs come with answers and explanations.
+          </p>
+        </div>
+      </div>
+
+    </div>
+
+<?php
+	include("Includes/footer.html");
+?>
